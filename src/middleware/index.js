ee4cf22ea7463a5af982f07d634dec3ff0f1ea50
Edinson CS <jean_clude_dortosms@hotmail.com>
2025-06-20 22:24:12 +0300

    Add simple frontend

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,12 +1,17 @@
+import express from 'express';
+import path from 'path';
+
 module.exports = (app) => {
 
-	//localhost:{$port}/api
-	app.use('/api', require('./Api'));
+        //localhost:{$port}/api
+        app.use('/api', require('./Api'));
 
+        // Static frontend files
+        app.use(express.static(path.join(__dirname, '../public')));
 
-	//localhost:{$port}/
-	app.use('/', require('./Frontend'));
+        //localhost:{$port}/
+        app.use('/', require('./Frontend'));
 
-	//app.use('/', require('./Frontend'));
-	//app.use('/backend', require('./Backend'));
+        //app.use('/', require('./Frontend'));
+        //app.use('/backend', require('./Backend'));
 }
