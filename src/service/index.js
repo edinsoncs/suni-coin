3be6a14ce38a6a70e0d81e3809c2e3324f86adec
Edinson CS <jean_clude_dortosms@hotmail.com>
2025-06-21 02:06:05 +0300

    Enable CORS for Next frontend

--- src/service/index.js
+++ src/service/index.js
@@ -17,6 +17,16 @@ global.newMiner = new Miner(newBlockchain, p2pAction, newWalletMiner);
 
 
 app.use(bodyParser.json());
+// Allow cross-origin requests so the Next.js frontend can call the API
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
 middleware(app);
 
 app.listen(PORT, () => {
