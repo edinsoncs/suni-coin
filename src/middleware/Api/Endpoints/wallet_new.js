1fc81f5bd0ab11c411fc8e58a7fb170604094eed
Edinson CS <jean_clude_dortosms@hotmail.com>
2025-06-21 02:40:19 +0300

    Revert "Fix wallet balance and extend explorer"

--- src/middleware/Api/Endpoints/wallet_new.js
+++ src/middleware/Api/Endpoints/wallet_new.js
@@ -1,19 +1,13 @@
-import Wallet, { Transaction, blockchainWallet } from '../../../wallet/index.js';
+import Wallet from '../../../wallet/index.js';
 
 
-export default (req, res) => {
+export default (req, res, next) => {
 
-        // create wallet with zero balance so funds come from the chain
-        global.wallet_new = new Wallet(newBlockchain, 0);
+	global.wallet_new = new Wallet(newBlockchain, 20);
 
-        // create funding transaction and mine it using the blockchain wallet
-        const initTx = Transaction.create(blockchainWallet, wallet_new.publicKey, 20);
-        const block = newBlockchain.addBlock([initTx], blockchainWallet);
-
-        res.json({
-                status: 'ok',
-                data: wallet_new.blockchainWallet(),
-                block
-        });
+	res.json({
+		status: 'ok',
+		data: wallet_new.blockchainWallet()
+	});
 
 };
